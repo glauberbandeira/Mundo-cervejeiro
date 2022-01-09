e6f1a124f4fc97e6ae96ae1f90d12c123714b2b5
glauberbandeira <glauberbandeira_@hotmail.com>
2022-01-09 19:01:30 -0300

    Enviando projeto

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Mundo Cervejeiro 👋
+
+Projeto criado 100% por mim, onde eu queria testar meu conhecimentos em JavaScript. Pela funcionalidades o projeto ficou conforme o planejado.
+
+Foi usado HTML, CSS, JavaScript e Responsivo.
---
+++ script.js
@@ -0,0 +1,184 @@
+function Becks() {
+  // Remover Classe Content
+  document.querySelector('#content').classList.remove('content');
+  document.querySelector('#budweiser').classList.remove('budweiser');
+  document.querySelector('#corona').classList.remove('corona');
+  document.querySelector('#heineken').classList.remove('heineken');
+  document.querySelector('#patagonia').classList.remove('patagonia');
+  document.querySelector('#sol').classList.remove('sol');
+  document.querySelector('#stella').classList.remove('stella');
+  // Romover Id content
+  document.querySelector('#content').style.display = 'none'; 
+  document.querySelector('#budweiser').style.display = 'none';
+  document.querySelector('#corona').style.display = 'none'; 
+  document.querySelector('#heineken').style.display = 'none'; 
+  document.querySelector('#patagonia').style.display = 'none'; 
+  document.querySelector('#sol').style.display = 'none';
+
+  // Adicionar Classe becks e Id becks
+  document.querySelector('#becks').classList.add('becks');
+  document.querySelector('#becks').style.display = 'grid';
+
+  // Mudando a cor de fundo e cor da letra
+  document.body.style.background = 'rgb(104, 126, 49)';
+  document.body.style.color = '#fff'
+}
+
+function Budweiser() {
+  // Remover Classe Content
+  document.querySelector('#content').classList.remove('content');
+  document.querySelector('#becks').classList.remove('becks');
+  document.querySelector('#corona').classList.remove('corona');
+  document.querySelector('#heineken').classList.remove('heineken');
+  document.querySelector('#patagonia').classList.remove('patagonia');
+  document.querySelector('#sol').classList.remove('sol');
+  document.querySelector('#stella').classList.remove('stella');
+  // Romover Id content
+  document.querySelector('#content').style.display = 'none'; 
+  document.querySelector('#becks').style.display = 'none'; 
+  document.querySelector('#corona').style.display = 'none'; 
+  document.querySelector('#heineken').style.display = 'none'; 
+  document.querySelector('#patagonia').style.display = 'none'; 
+  document.querySelector('#sol').style.display = 'none';
+
+  // Adicionar Classe becks e Id becks
+  document.querySelector('#budweiser').classList.add('budweiser');
+  document.querySelector('#budweiser').style.display = 'grid';
+
+  // Mudando a cor de fundo e cor da letra
+  document.body.style.background = 'rgb(203, 24, 27)';
+  document.body.style.color = '#fff'
+}
+
+function Corona() {
+  // Remover Classe Content
+  document.querySelector('#content').classList.remove('content');
+  document.querySelector('#becks').classList.remove('becks');
+  document.querySelector('#budweiser').classList.remove('budweiser');
+  document.querySelector('#heineken').classList.remove('heineken');
+  document.querySelector('#patagonia').classList.remove('patagonia');
+  document.querySelector('#sol').classList.remove('sol');
+  document.querySelector('#stella').classList.remove('stella');
+  // Romover Id content
+  document.querySelector('#content').style.display = 'none'; 
+  document.querySelector('#becks').style.display = 'none'; 
+  document.querySelector('#budweiser').style.display = 'none'; 
+  document.querySelector('#heineken').style.display = 'none'; 
+  document.querySelector('#patagonia').style.display = 'none'; 
+  document.querySelector('#sol').style.display = 'none';
+  document.querySelector('#stella').style.display = 'none';
+
+  // Adicionar Classe becks e Id becks
+  document.querySelector('#corona').classList.add('corona');
+  document.querySelector('#corona').style.display = 'grid';
+
+  // Mudando a cor de fundo e cor da letra
+  document.body.style.background = 'rgb(1, 26, 67)';
+  document.body.style.color = '#fff'
+}
+
+function Heineken() {
+  // Remover Classe Content
+  document.querySelector('#content').classList.remove('content');
+  document.querySelector('#becks').classList.remove('becks');
+  document.querySelector('#budweiser').classList.remove('budweiser');
+  document.querySelector('#corona').classList.remove('corona');
+  document.querySelector('#patagonia').classList.remove('patagonia');
+  // Romover Id content
+  document.querySelector('#content').style.display = 'none'; 
+  document.querySelector('#becks').style.display = 'none'; 
+  document.querySelector('#budweiser').style.display = 'none'; 
+  document.querySelector('#corona').style.display = 'none';
+  document.querySelector('#patagonia').style.display = 'none';
+
+  // Adicionar Classe becks e Id becks
+  document.querySelector('#heineken').classList.add('heineken');
+  document.querySelector('#heineken').style.display = 'grid';
+
+  // Mudando a cor de fundo e cor da letra
+  document.body.style.background = 'rgb(25, 179, 4)';
+  document.body.style.color = '#fff'
+}
+
+
+function Patagonia() {
+  // Remover Classe Content
+  document.querySelector('#content').classList.remove('content');
+  document.querySelector('#becks').classList.remove('becks');
+  document.querySelector('#budweiser').classList.remove('budweiser');
+  document.querySelector('#corona').classList.remove('corona');
+  document.querySelector('#heineken').classList.remove('heineken');
+  document.querySelector('#sol').classList.remove('sol');
+  document.querySelector('#stella').classList.remove('stella');
+  // Romover Id content
+  document.querySelector('#content').style.display = 'none'; 
+  document.querySelector('#becks').style.display = 'none'; 
+  document.querySelector('#budweiser').style.display = 'none'; 
+  document.querySelector('#corona').style.display = 'none'; 
+  document.querySelector('#heineken').style.display = 'none'; 
+  document.querySelector('#sol').style.display = 'none'; 
+  document.querySelector('#stella').style.display = 'none'; 
+
+  // Adicionar Classe becks e Id becks
+  document.querySelector('#patagonia').classList.add('patagonia');
+  document.querySelector('#patagonia').style.display = 'grid';
+
+  // Mudando a cor de fundo e cor da letra
+  document.body.style.background = 'rgb(123, 119, 110)';
+  document.body.style.color = '#fff'
+
+}
+
+function Sol() {
+  // Remover Classe Content
+  document.querySelector('#content').classList.remove('content');
+  document.querySelector('#becks').classList.remove('becks');
+  document.querySelector('#budweiser').classList.remove('budweiser');
+  document.querySelector('#corona').classList.remove('corona');
+  document.querySelector('#heineken').classList.remove('heineken');
+  document.querySelector('#patagonia').classList.remove('patagonia');
+  document.querySelector('#stella').classList.remove('stella');
+  // Romover Id content
+  document.querySelector('#content').style.display = 'none'; 
+  document.querySelector('#becks').style.display = 'none'; 
+  document.querySelector('#budweiser').style.display = 'none'; 
+  document.querySelector('#corona').style.display = 'none'; 
+  document.querySelector('#heineken').style.display = 'none'; 
+  document.querySelector('#patagonia').style.display = 'none'; 
+  document.querySelector('#stella').style.display = 'none'; 
+
+  // Adicionar Classe becks e Id becks
+  document.querySelector('#sol').classList.add('sol');
+  document.querySelector('#sol').style.display = 'grid';
+
+  // Mudando a cor de fundo e cor da letra
+  document.body.style.background = 'rgb(205, 84, 67)';
+  document.body.style.color = '#fff'
+}
+
+function Stella() {
+  // Remover Classe Content
+  document.querySelector('#content').classList.remove('content');
+  document.querySelector('#becks').classList.remove('becks');
+  document.querySelector('#budweiser').classList.remove('budweiser');
+  document.querySelector('#corona').classList.remove('corona');
+  document.querySelector('#heineken').classList.remove('heineken');
+  document.querySelector('#patagonia').classList.remove('patagonia');
+  document.querySelector('#sol').classList.remove('sol');
+  // Romover Id content
+  document.querySelector('#content').style.display = 'none'; 
+  document.querySelector('#becks').style.display = 'none'; 
+  document.querySelector('#budweiser').style.display = 'none'; 
+  document.querySelector('#corona').style.display = 'none'; 
+  document.querySelector('#heineken').style.display = 'none'; 
+  document.querySelector('#patagonia').style.display = 'none'; 
+  document.querySelector('#sol').style.display = 'none'; 
+
+  // Adicionar Classe becks e Id becks
+  document.querySelector('#stella').classList.add('stella');
+  document.querySelector('#stella').style.display = 'grid';
+
+  // Mudando a cor de fundo e cor da letra
+  document.body.style.background = 'rgb(149, 198, 18)';
+  document.body.style.color = '#fff'
+}
